d5e1451627a80b92ea9c612dcbb4d1696e3e8cbe
semyers189 <semyers@fullsail.edu>
2020-10-22 21:56:36 -0400

    created server

---
+++ server.js
@@ -0,0 +1,12 @@
+const log = require('debug')('server:log');
+const express = require('express');
+
+const app = express();
+
+//define ports
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  log(`Server is listening on port ${PORT}`);
+})
+
