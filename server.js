74601dc1a4d08ca1780c4829684d38750867ed15
semyers189 <semyers@fullsail.edu>
2020-10-30 23:24:17 -0400

    added bodyparser stuff to serverside

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
 const index = require('./routes/index');
