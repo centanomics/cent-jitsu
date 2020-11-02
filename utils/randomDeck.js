173b2f8b307f9ec03d6a033977ba02cc41a162aa
semyers189 <semyers@fullsail.edu>
2020-11-01 23:19:32 -0500

    created deck and shuffle deck function

--- utils/randomDeck.js
+++ utils/randomDeck.js
@@ -3,9 +3,29 @@ const colors = ["BA2D0B", "3EC300", "000500", "B084CC", "2F52E0", "3A405A"]
 //numbers 2-12
 // 66
 
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 const randomDeck = () => {
   let returnDeck = [];
 
+  for (let i = 0; i < elements.length; i++) {
+    for (let j = 0; j < colors.length; j++) {
+      for (let k = 2; k < 13; k++) {
+        returnDeck.push({
+          element: elements[i],
+          color: colors[j],
+          number: k
+        })
+      }
+    }
+  }
+
+  shuffle(returnDeck);
   return returnDeck;
 }
 
