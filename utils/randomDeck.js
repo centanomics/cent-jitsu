e4d1420bbf7745202cba508431d7c0afe5fb0c94
semyers189 <semyers@fullsail.edu>
2020-11-01 22:53:57 -0500

    actually setup randomdeck function with returnarray

--- utils/randomDeck.js
+++ utils/randomDeck.js
@@ -1,5 +1,12 @@
-const randomDeck = () => {
+const elements = ["coin", "bill", "gold"]
+const colors = ["BA2D0B", "3EC300", "000500", "B084CC", "2F52E0", "3A405A"]
+//numbers 2-12
+// 66
 
+const randomDeck = () => {
+  let returnDeck = [];
+
+  return returnDeck;
 }
 
 module.exports = randomDeck
