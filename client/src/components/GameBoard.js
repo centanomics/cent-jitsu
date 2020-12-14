16c8cd9b85cdac50309a02f177c671d271523c0f
semyers189 <semyers@fullsail.edu>
2020-12-13 23:31:54 -0500

    restarted the gameboard component

--- client/src/components/GameBoard.js
+++ client/src/components/GameBoard.js
@@ -7,31 +7,34 @@ import '../styles/App.css'
 
 const GameBoard = ({gameId}) => {
 
-  const [players, setPlayers] = useState([]);
-  const [playerId, setPlayerId] = useState('');
-  const [people, setPeople] = useState([]);
+  // const [players, setPlayers] = useState([]);
+  // const [playerId, setPlayerId] = useState('');
+  // const [people, setPeople] = useState([]);
 
-  useEffect(() => {
-    getPlayerId((id) => setPlayerId(id));
-    subscribeToTimer(1000, setPlayers((null, peeps => peeps.filter(player => player.gameId === gameId))))
-    const tempArr = []
-    tempArr.push(players.filter(player => player.id !== playerId)[0])
-    tempArr.push(players.filter(player => player.id === playerId)[0])
-    console.log(players)
-    setPeople(tempArr)
-    // eslint-disable-next-line 
-  }, [])
+  // useEffect(() => {
+  //   getPlayerId((id) => setPlayerId(id));
+  //   subscribeToTimer(1000, setPlayers((null, peeps => peeps.filter(player => player.gameId === gameId))))
+  //   const tempArr = []
+  //   tempArr.push(players.filter(player => player.id !== playerId)[0])
+  //   tempArr.push(players.filter(player => player.id === playerId)[0])
+  //   console.log(players)
+  //   setPeople(tempArr)
+  //   // eslint-disable-next-line 
+  // }, [])
+  // return (
+  //   <div className="gameBoard">
+  //     {people.map((player, i) => {
+  //       return (
+  //         <div key={`${player.id}`} className={`player${i + 1}`} >
+  //           <Deck playerId={player.id} cards={player.deck} />
+  //           {player.id === playerId ? <p>This is you</p> : null}
+  //         </div>
+  //       )
+  //     })}
+  //   </div>
+  // )
   return (
-    <div className="gameBoard">
-      {people.map((player, i) => {
-        return (
-          <div key={`${player.id}`} className={`player${i + 1}`} >
-            <Deck playerId={player.id} cards={player.deck} />
-            {player.id === playerId ? <p>This is you</p> : null}
-          </div>
-        )
-      })}
-    </div>
+    <div>Gameboard</div>
   )
 }
 
